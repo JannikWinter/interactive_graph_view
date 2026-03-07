ffdd8062cfec0a0713331dc1ead80a205733ca8f
Jannik Winter <kontakt@jannik-winter.com>
2026-03-07 23:08:32 +0100

    add show modifier to exports

--- lib/graph_view.dart
+++ lib/graph_view.dart
@@ -1,18 +1,32 @@
-export "src/drag_details.dart";
-export "src/graph_viewport_behavior.dart";
-export "src/graph_viewport_controller.dart";
-export "src/graph_viewport_transform.dart";
-export "src/graph_visibility.dart";
-export "src/interaction_config.dart";
-export "src/node_overlay.dart";
-export "src/widgets/edge.dart";
-export "src/widgets/graph_view.dart";
-export "src/widgets/graph_viewport.dart";
-export "src/widgets/node.dart";
-export "src/style/arrow_style.dart";
-export "src/style/curve_style.dart";
-export "src/style/edge_style.dart";
-export "src/style/line_shadow.dart";
-export "src/style/line_style.dart";
-export "src/style/node_style.dart";
-export "src/style/graph_style.dart";
+export "src/drag_details.dart"
+    show NodeDragDownDetails, NodeDragStartDetails, NodeDragUpdateDetails, NodeDragEndDetails;
+export "src/graph_viewport_behavior.dart"
+    show
+        GraphViewportMoveBehavior,
+        GraphViewportZoomBehavior,
+        GraphViewportZoomToFitBehavior,
+        GraphViewportZoomToScaleBehavior,
+        GraphViewportBehaviorResolver;
+export "src/graph_viewport_controller.dart" show GraphViewportController, NodesMovedCallback;
+export "src/graph_viewport_transform.dart" show GraphViewportTransform, TransformSettleListener;
+export "src/graph_visibility.dart" show GraphVisibility;
+export "src/interaction_config.dart" show InteractionConfig, CameraEdgeMoveConfig;
+export "src/node_overlay.dart" show NodeOverlay;
+export "src/widgets/edge.dart" show EdgeWidget;
+export "src/widgets/graph_view.dart" show GraphView, GraphViewState;
+export "src/widgets/graph_viewport.dart" show GraphViewport;
+export "src/widgets/node.dart"
+    show
+        NodeWidget,
+        GestureNodeDragDownCallback,
+        GestureNodeDragStartCallback,
+        GestureNodeDragUpdateCallback,
+        GestureNodeDragEndCallback,
+        GestureNodeDragCancelCallback;
+export "src/style/arrow_style.dart" show ArrowStyle;
+export "src/style/curve_style.dart" show CurveStyle, StraightCurveStyle;
+export "src/style/edge_style.dart" show EdgeStyle;
+export "src/style/line_shadow.dart" show LineShadow;
+export "src/style/line_style.dart" show LineStyle, SolidLineStyle, DashedLineStyle, DottedLineStyle;
+export "src/style/node_style.dart" show NodeStyle;
+export "src/style/graph_style.dart" show GraphStyle;
