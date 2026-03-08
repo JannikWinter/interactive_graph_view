0ae2ac43f1c5a30db0bb39b3e228aee5f853e572
Jannik Winter <kontakt@jannik-winter.com>
2026-03-08 18:14:08 +0100

    add documentation for util classes

--- lib/src/util/extensions.dart
+++ lib/src/util/extensions.dart
@@ -1,14 +1,24 @@
 import "package:flutter/painting.dart" show Size;
 import "package:flutter/rendering.dart" show Rect;
 
+/// An extension that adds [isNotEmpty] to [Rect].
 extension NotEmptyRect on Rect {
+  /// Whether this rect is not empty.
+  ///
+  /// Precisely this returns true when the horizontal **and** the vertical size are both positive - zero exclusive.
   bool get isNotEmpty => top < bottom && left < right;
 }
 
+/// An extension that adds [isNotEmpty] to [Size].
 extension NotEmptySize on Size {
+  /// Whether this size is not empty.
+  ///
+  /// Precisely this returns true when [width] **and** [height] are both larger than `0.0`.
   bool get isNotEmpty => width > 0.0 && height > 0.0;
 }
 
+/// An extension that adds [whereNot] to [Iterable].
 extension IterableWhereNot<E> on Iterable<E> {
+  /// Returns a new `Iterable` for which the given [test] evaluates to `false`.
   Iterable<E> whereNot(bool Function(E element) test) => where((element) => !test(element));
 }
--- lib/src/util/nullable.dart
+++ lib/src/util/nullable.dart
@@ -1,8 +1,25 @@
 import "package:flutter/foundation.dart";
 
+/// A type used to be able to overwrite nullable values in e.g. `copyWith()` methods.
+///
+/// Usually in a `copyWith(...)` method, all parameters are nullable and `null` by default. If you supply a different
+/// value than `null` for any parameter, the respective field is replaced in the copy.
+///
+/// This is not possible for nullable fields. We would never be able to overwrite this field by `null` in the copy.
+///
+/// That's where this type comes in. A nullable field is exposed as follows:
+/// ```dart
+/// T copyWith({Nullable<int>? nullableValue}) {...}
+/// ```
+/// When you do not want to replace the value in the copy you just supply `null` as usual.
+///
+/// When you instead want to replace the value in the copy, you supply `Nullable(newValue)`, where `newValue` can also
+/// be null.
 @immutable
 class Nullable<ValueType extends Object> {
+  /// Constructs a new nullable of a given [value].
   const Nullable(this.value);
 
+  /// The nullable value.
   final ValueType? value;
 }
